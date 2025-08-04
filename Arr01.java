6a64afb64eb3c227ec6c32c60f7d2e9cac098c81
kishan <kk1085035@gmail.com>
2025-08-04 21:42:26 +0530

    day end

--- Arr01.java
+++ Arr01.java
@@ -3,5 +3,13 @@ class Arr01{
         int[] arr = new int[10];
         arr[0]= 98;
         arr[1]= 23;
+          // directly
+
+        int[] roll_no = {4,6,5,9,444,45,1,};
+
+        int[] name ; // declaration of array
+        name = new int[34];//initilization of array
+
+        System.out.println(arr[1]);
     }
 }
---
+++ Excep.java
@@ -0,0 +1,7 @@
+public class Excep {
+ public static void main(String[] args) {
+    //syntax      logical    runtime
+    
+ }
+    
+}
