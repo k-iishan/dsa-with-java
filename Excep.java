67175ff3168672473cbb55f95f367638578e09d7
kishan <kk1085035@gmail.com>
2025-08-05 09:01:50 +0530

    try and catch

--- Excep.java
+++ Excep.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Excep {
  public static void main(String[] args) {
     //syntax      logical    runtime
+
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    int b = input.nextInt();
+    try { System.out.println(a/b);
+      
+    } catch (ArithmeticException e) {
+      System.out.println(e);
+      System.out.println("plese check the format");
+    }
+   
     
  }
     
